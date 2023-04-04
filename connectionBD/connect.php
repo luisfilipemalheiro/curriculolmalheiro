f8bb29d3e98520d578be2e12beed0ec02ca792d6
tiagov02 <96255633+tiagov02@users.noreply.github.com>
2023-04-04 16:09:33 +0100

    Update connect.php

--- connectionBD/connect.php
+++ connectionBD/connect.php
@@ -18,9 +18,9 @@ catch(PDOException $e) {
 
 function pdo_connect_mysql(){
     $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'curriculoluis';
+    $DATABASE_USER = 'sir';
+    $DATABASE_PASS = 'sir';
+    $DATABASE_NAME = 'SIR';
 
     try{
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME. ';charset=utf8',
